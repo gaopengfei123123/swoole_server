6f9ed137131421cb671690f55871f5ed2bf3461e
gaofeifiy <5173180@qq.com>
2017-03-03 16:43:12 +0800

    添加composer文件 init

---
+++ src/Hello.php
@@ -0,0 +1,16 @@
+<?php
+namespace SServer;
+
+/**
+ * Created by PhpStorm.
+ * User: gaopengfei
+ * Date: 17/3/3
+ * Time: 下午4:11
+ */
+class Hello
+{
+    public static function say(){
+        return '1231232 hello';
+    }
+
+}
